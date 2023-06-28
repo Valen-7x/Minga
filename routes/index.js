130f63b91f5a7e20c803da13c18f54d99c764269
ValentinDorigo <115356217+Valen-7x@users.noreply.github.com>
2023-06-28 13:19:34 -0300

    Minga 08 (#7)
    * minga-03
    
    * minga-6
    
    * minga7
    
    * minga08

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-import express from 'express';
-let router = express.Router();
+import express from 'express'
+import author_router from './authors.js'
+import category_router from './categories.js'
+import manga_router from './mangas.js'
+import chapter_router from './chapters.js'
+import auth_router from './users.js'
 
-/* GET home page. */
+let router = express.Router()
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/authors' ,author_router) //obligo al enrutador principal a usar las rutas de autores con la palabrita authors
+router.use('/categories' ,category_router)
+router.use('/mangas' ,manga_router)
+router.use('/chapters' ,chapter_router)
+router.use('/auth' ,auth_router)
 
 export default router
