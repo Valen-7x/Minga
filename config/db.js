fb2ad3bd6c207bf292072fb432d87b08bc6ad9a2
Valen-7x <valen.deorigen@gmail.com>
2023-06-28 11:31:00 -0300

    minga-03

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 import {connect} from "mongoose";
-connect (process.env.LINK_DB)
-.then(()=>console.log("connected to data base"))
+connect (process.env.LINK_DB)  //conecto con la base de datos -.
+.then(()=>console.log("connected to data base")) //devuelve una promesa
 .catch((error)=>(console.log(error)))
